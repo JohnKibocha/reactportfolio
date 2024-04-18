e1c8491f5a1b32a566e80c005bd680fa10a90c45
JohnKibocha <90314729+JohnKibocha@users.noreply.github.com>
2024-04-18 10:04:58 +0300

    third commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -184,7 +184,7 @@ const Navbar = () => {
 
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-slate-200 rounded-e-2xl font-semibold">
             <a
-              href="johnkibocha@outlook.com"
+                href="mailto:johnkibocha@outlook.com"
               className="flex justify-between items-center w-full text-[#3A1078]"
             >
               Email <HiMail size={30} />
